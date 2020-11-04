598998819f04e95fc937286b42e4b4286dd827fe
Eugene Gordievsky <eugene.gordievsky@yandex.ru>
2020-11-04 14:21:32 +0300

    feat: получение карточек и информации пользователя из запросов

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,7 @@ export default class Card {
     this._cardSelector = cardSelector;
     this._name = data.name;
     this._link = data.link;
+    this._likes = data.likes;
     this._handleCardClick = handleCardClick;
     this._popupCloseButton = popupCloseButton;
   };
@@ -17,9 +18,11 @@ export default class Card {
     this._elementImage = this._element.querySelector(".element__image");
     this._setEventListeners();
 
+
     this._elementImage.src = this._link;
     this._elementImage.alt = this._name;
     this._element.querySelector(".element__title").textContent = this._name;
+    this._element.querySelector(".element__like-counter").textContent = this._likes.length;
 
     return this._element;
   };
@@ -27,7 +30,8 @@ export default class Card {
   _setEventListeners() {
     this._elementImage.addEventListener("click", this._handleCardClick);
     this._popupCloseButton.addEventListener("click", () => this._handleClosePopup());
-    this._element.querySelector(".element__like").addEventListener("click", (evt) => evt.target.classList.toggle("element__like_active"));
+    this._element.querySelector(".element__like-button").addEventListener("click",
+    (evt) => evt.target.classList.toggle("element__like-button_active"));
     this._element.querySelector(".element__delete-button").addEventListener("click", (evt) => {
       const element = evt.target.closest(".element");
       element.remove();
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 import { editButton, popupEdit, popupAdd, popupFullImage, addButton, popupEditForm, popupAddForm,
-profileName, profileJob, inputTitle, inputSrc, formClasses, initialCards, fullImageSrc, fullImageTitle,
-popupEnterJob, popupEnterName, closeImageButton} from "../utils/constants.js";
+profileName, profileJob, formClasses, fullImageSrc, fullImageTitle, avatarButton,
+popupEnterJob, popupEnterName, closeImageButton, profileAvatar, popupEditAvatar, popupAvatarForm} from "../utils/constants.js";
 import { Section } from "../components/Section.js";
 
 const newCard = (item) => {
@@ -18,11 +18,8 @@ const newCard = (item) => {
 const imagePopup = new PopupWithImage(popupFullImage, fullImageSrc, fullImageTitle);
 const validEditForm = new FormValidator(formClasses, popupEditForm);
 const validAddForm = new FormValidator(formClasses, popupAddForm);
+const validAvatarForm = new FormValidator(formClasses, popupAvatarForm);
 const userInfo = new UserInfo(profileName, profileJob);
-const initCard = new Section({
-  items: initialCards,
-  renderer: (item) => initCard.addItem(newCard(item), initialCards)},
-  ".elements");
 const editPopup = new PopupWithForm(popupEdit, (item) => {
   userInfo.setUserInfo(item);
   editPopup.close();
@@ -32,15 +29,19 @@ const addCardPopup = new PopupWithForm(popupAdd, (item) => {
 
   addCardPopup.close();
 });
+const avatarPopup = new PopupWithForm(popupEditAvatar, (item) => {
+  console.log(item);
+});
 
-initCard.renderItems();
 
 imagePopup.setEventListeners();
 editPopup.setEventListeners();
 addCardPopup.setEventListeners();
+avatarPopup.setEventListeners();
 
 validEditForm.enableValidation();
 validAddForm.enableValidation();
+validAvatarForm.enableValidation();
 
 editButton.addEventListener("click", () => {
   const user = userInfo.getUserInfo();
@@ -54,3 +55,45 @@ addButton.addEventListener("click", () => {
   validAddForm.clearError();
   addCardPopup.open();
 });
+
+avatarButton.addEventListener("click", () => {
+  avatarPopup.open();
+})
+
+fetch("https://mesto.nomoreparties.co/v1/cohort-17/cards", {
+    headers: {
+      authorization: "761b2eed-9b1d-4590-81a1-cb393f952eaa"
+    }
+  })
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Произошла ошибка: ${res.status}:${res.statusText}`)
+  })
+  .then((result) => {
+    const initCardServer = new Section({
+      items: result,
+      renderer: (item) => initCardServer.addItem(newCard(item), result)},
+      ".elements");
+      initCardServer.renderItems()})
+  .catch((err) => {
+    console.log(err);
+  })
+
+fetch("https://mesto.nomoreparties.co/v1/cohort-17/users/me", {
+  headers: {
+    authorization: "761b2eed-9b1d-4590-81a1-cb393f952eaa"
+  }
+})
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Произошла ошибка: ${res.status}:${res.statusText}`)
+  })
+  .then((result) => {
+    profileName.textContent = result.name;
+    profileJob.textContent = result.about;
+    profileAvatar.src = result.avatar;
+  })
